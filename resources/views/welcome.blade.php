9d1ff17b8a6e1117d4b56c532e9a7c796a97af34
mochammadtohari30 <Mochammadtohari30@gmail.com>
2023-08-28 09:46:44 +0700

    merapihkan layout admin & wellcome
    merapihkan layout admin & wellcome
    
    menonaktifkan sebagian menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,10 +82,10 @@
                     <h4><strong>Indeks Data Murid Madrasah</strong></h4>
                     <span class="info-box-number-1 mb-3">
                         <p class="info-box-text-madrasah">Jumlah Data murid: {{ $data_murid_kondisi['data_murid_jumlah'] }}</p>
-                        <p class="info-box-text-aktif">murid Aktif: {{ $data_murid_kondisi['murid_aktif_count'] }}</p>
-                        <p class="info-box-text-tidak-aktif">murid Tidak Aktif: {{ $data_murid_kondisi['murid_tidak_aktif_count'] }}</p>
-                        <p class="info-box-text-lainya">murid Lainya: {{ $data_murid_kondisi['murid_pindah_count'] }}</p>
-                        <p class="info-box-text-lainya">murid Lainya: {{ $data_murid_kondisi['murid_lainya_count'] }}</p>
+                        <p class="info-box-text-aktif">Murid Aktif: {{ $data_murid_kondisi['murid_aktif_count'] }}</p>
+                        <p class="info-box-text-tidak-aktif">Murid Tidak Aktif: {{ $data_murid_kondisi['murid_tidak_aktif_count'] }}</p>
+                        <p class="info-box-text-lainya">Murid Pindah: {{ $data_murid_kondisi['murid_pindah_count'] }}</p>
+                        <p class="info-box-text-lainya">Murid Lainya: {{ $data_murid_kondisi['murid_lainya_count'] }}</p>
                       {{-- <small>Data</small> --}}
                     </span>
                   </div>
@@ -120,56 +120,56 @@
 
           </div>
 
-        <div class="row">
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+        {{-- <div class="row">
+            <!-- /.col -->
+            <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
-            <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">41,410</span>
+                <div class="info-box-content">
+                <span class="info-box-text">Likes</span>
+                <span class="info-box-number">41,410</span>
+                </div>
+                <!-- /.info-box-content -->
             </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
-        <div class="clearfix hidden-md-up"></div>
-
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Sales</span>
-              <span class="info-box-number">760</span>
+            <!-- /.info-box -->
             </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+            <!-- /.col -->
 
-            <div class="info-box-content">
-              <span class="info-box-text">New Members</span>
-              <span class="info-box-number">2,000</span>
+            <!-- fix for small devices only -->
+            <div class="clearfix hidden-md-up"></div>
+
+            <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">Sales</span>
+                <span class="info-box-number">760</span>
+                </div>
+                <!-- /.info-box-content -->
             </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
+            <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+                <div class="info-box-content">
+                <span class="info-box-text">New Members</span>
+                <span class="info-box-number">2,000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+            </div>
         <!-- /.col -->
-      </div>
+      </div> --}}
       <!-- /.row -->
 
 
-      <div class="row">
+      {{-- <div class="row">
         <div class="col-md-12">
           <div class="card">
             <div class="card-header">
@@ -302,11 +302,11 @@
           <!-- /.card -->
         </div>
         <!-- /.col -->
-      </div>
+      </div> --}}
       <!-- /.row -->
 
       <!-- Main row -->
-      <div class="row">
+      {{-- <div class="row">
         <!-- Left col -->
         <div class="col-md-8">
           <!-- MAP & BOX PANE -->
@@ -932,7 +932,7 @@
           <!-- /.card -->
         </div>
         <!-- /.col -->
-      </div>
+      </div> --}}
       <!-- /.row -->
     </div><!--/. container-fluid -->
   </section>

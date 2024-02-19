7255c2e53e1ce971187d8d83e05d3d69b4188dcc
Mochammad-tohari <32976741+Mochammad-tohari@users.noreply.github.com>
2024-02-19 15:05:04 +0700

    pembenahan file admin.blade.php
    perubahan nama right sidebar menjadi customize theme

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -43,7 +43,7 @@
                 </li>
 
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
+                    <a href="#" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="#" class="nav-link">Contact</a>
